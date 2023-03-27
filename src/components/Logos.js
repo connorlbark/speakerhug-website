f2fab7d1c946fe21aa477f5db54b0fd444ba228d
Notgnihsaw <52753287+Notgnihsaw@users.noreply.github.com>
2023-03-27 14:53:00 -0400

    un-deleted angel

--- src/components/Logos.js
+++ src/components/Logos.js
@@ -4,6 +4,7 @@ export function Logos() {
   return (
     <div>
       <img src={require('../images/logos/text.png')} className="logo glitch" alt="speakerhug logo"/>
+      <img src={require('../images/logos/homepage_angel.PNG')} className="angel" alt=""/>
     </div>
   )
 }
